81d201931b126f67d42ab221f1509d551f6450c5
Marie <mariedaily444@gmail.com>
2020-04-22 11:45:48 -0700

    Added 'attending to a gig'  feature

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,39 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var attendeeId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == attendeeId && a.GigId == dto.GigId))
+                return BadRequest("The attendance already exists.");
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = attendeeId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
