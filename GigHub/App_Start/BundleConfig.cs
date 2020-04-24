9720b028d807b3c984d74fcfa01f084934735883
Marie <mariedaily444@gmail.com>
2020-04-24 08:57:14 -0700

    Implemented logical deletion of gigs

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -19,11 +19,14 @@ namespace GigHub
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.min.js",
+                      "~/Scripts/bootbox.locales.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
